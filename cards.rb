c840744e99685669af257c14f7f4a18785fd8450
Joe Alba <joealba@JoeBookPro.local>
2015-10-05 20:54:35 -0400

    Passing spec: Card can see the card definition info

--- cards.rb
+++ cards.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 require 'rspec'
 
-
 module CardDefinition
   SUITS = ['diamonds', 'clubs', 'spades', 'hearts']
   VALUES = (2..10).to_a + ['J','Q','K','A']
@@ -13,6 +12,9 @@ end
 
 
 RSpec.describe "cards" do
-
+  it "can access the CardDefinition constants" do
+    expect(Card::SUITS.is_a? Array).to eq true
+    expect(Card::SUITS.length).to eq 4
+  end
 end
 
